4310303ab3f971535553e02c6dc770a454b2afbf
Rastislav <rastislav.kucera@vacuumlabs.com>
2019-07-09 12:59:31 +0200

    Updates

---
+++ precache-manifest.c458dc8af184dc331d3918b25c95d900.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c280c53f4d8f6647c18ad710d8c754da",
+    "url": "/image-browser/index.html"
+  },
+  {
+    "revision": "7eb183224da53d44ca45",
+    "url": "/image-browser/static/css/main.80c08146.chunk.css"
+  },
+  {
+    "revision": "195b19bc586506e11219",
+    "url": "/image-browser/static/js/2.0a2a832b.chunk.js"
+  },
+  {
+    "revision": "7eb183224da53d44ca45",
+    "url": "/image-browser/static/js/main.d70ead93.chunk.js"
+  },
+  {
+    "revision": "bc81fb6733d5eda6015b",
+    "url": "/image-browser/static/js/runtime~main.dce8cfed.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/image-browser/precache-manifest.1d86bd35b5c47cb86f99999973443d1f.js"
+  "/image-browser/precache-manifest.c458dc8af184dc331d3918b25c95d900.js"
 );
 
 self.addEventListener('message', (event) => {
